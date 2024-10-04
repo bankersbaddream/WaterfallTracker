df35911338954361b2e0e1581b647b56b700b548
bankersbaddream <29845448+bankersbaddream@users.noreply.github.com>
2024-10-04 10:34:35 -0400

    Create styles.js

---
+++ styles/styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  map: {
+    height: 300,
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
+    marginBottom: 16,
+  },
+});
+
+export default styles;
